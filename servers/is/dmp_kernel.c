03af1cd5b68bb17caac5e65dc9d3a268bf252a56
klemons <31633814+klemons@users.noreply.github.com>
2018-10-08 00:25:49 -0500

    Update dmp_kernel.c

--- servers/is/dmp_kernel.c
+++ servers/is/dmp_kernel.c
@@ -494,7 +494,7 @@ int proc_nr;
 		 return;
 	 
 	 printf("---------------- Matthew, John, Kyle - Message Table Dump ---------------- [%d/%d]\n", cs, END_PROC_ADDR-BEG_PROC_ADDR);
-	 printf("%9s|", "X \\ Y");
+	 printf("%7s|", "X \\ Y");
 	 
 	 for (rp = BEG_PROC_ADDR; rp < END_PROC_ADDR, p < 9; rp++)
 	 {
@@ -504,7 +504,7 @@ int proc_nr;
 		 if (j < cs)
 			 continue;
 		 ++p;
-		 printf("%9s|", rp->p_name);
+		 printf("%7s|", rp->p_name);
 	 }
 	 printf("\n");
 	 
@@ -517,7 +517,7 @@ int proc_nr;
 			 continue;
 		 ++s;
 		 j = p = 0;
-		 printf("%9s|", rp->p_name);
+		 printf("%7s|", rp->p_name);
 		 for (i = 0; i < NR_TASKS + NR_PROCS, p < 9; i++)
 		 {
 			if (isemptyp(&proc[i]))
@@ -526,7 +526,7 @@ int proc_nr;
 		 	if (j < cs)
 			 continue;
 			++p;
-			printf("%9d|", rp->os_message_table[i]);
+			printf("%7d|", rp->os_message_table[i]);
 		 }
 		 printf("\n");
 	 }
